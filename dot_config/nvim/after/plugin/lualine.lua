f85ae7f226142325cf67ab5517fe1455ac49c13b
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2023-04-04 17:59:21 +0100

    Update .config/nvim/after/plugin/lualine.lua

--- dot_config/nvim/after/plugin/lualine.lua
+++ dot_config/nvim/after/plugin/lualine.lua
@@ -4,7 +4,7 @@ require('lualine').setup {
   options = {
     icons_enabled = false,
     theme = 'rose-pine',
-    theme = 'onedark',
+    -- theme = 'onedark',
     component_separators = '|',
     section_separators = '',
   },
