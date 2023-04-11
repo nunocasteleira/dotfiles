f84a78bc323dcf1f8b9c5f03985a60ee96367c89
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2023-04-11 15:45:44 +0100

    Update .config/kitty/kitty.conf Add .config/nvim/after/plugin/zen-mode.lua

---
+++ dot_config/nvim/after/plugin/zen-mode.lua
@@ -0,0 +1,15 @@
+local zen = require("zen-mode")
+
+zen.setup({
+	twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+	gitsigns = { enabled = false }, -- disables git signs_defined
+
+	-- this will change the font size on kitty when in zen mode
+	-- to make this work, you need to set the following kitty options:
+	-- - allow_remote_control socket-only
+	-- - listen_on unix:/tmp/kitty
+	kitty = {
+		enabled = true,
+		font = "+4", -- font size increment
+	},
+})
