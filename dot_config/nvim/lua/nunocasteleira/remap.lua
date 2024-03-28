1d4f0aaeea3455be2dccec649f6c91a4201631f0
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2024-03-28 22:44:47 +0000

    Update .config/nvim/lua/nunocasteleira/remap.lua

--- dot_config/nvim/lua/nunocasteleira/remap.lua
+++ dot_config/nvim/lua/nunocasteleira/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>vp", vim.cmd.Vex)
 
@@ -17,7 +17,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- yank to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -26,4 +26,3 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
