f0b7f4ac4a5727fe1b5d0a2244a5785a0a5b53d6
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2024-03-28 23:50:17 +0000

    Add .config/nvim/lua/plugins/test.lua Remove .config/nvim/plugin/packer_compiled.lua

---
+++ dot_config/nvim/lua/plugins/test.lua
@@ -0,0 +1,20 @@
+return {
+  { "vim-neotest/neotest-jest" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-jest"] = {
+          args = {
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          },
+        },
+      },
+    },
+  },
+}
