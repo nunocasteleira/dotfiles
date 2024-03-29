10a935b3263414f91280db617815f6ffe27bbd9f
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2024-03-29 00:06:52 +0000

    Update .config/nvim/lua/plugins/test.lua

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -7,7 +7,16 @@ return {
         ["neotest-jest"] = {
           args = {
             jestCommand = "npm test --",
-            jestConfigFile = "custom.jest.config.ts",
+            jestConfigFile = function(file)
+              if string.find(file, "/packages/") then
+                return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+              end
+              if string.find(file, "apps/") then
+                return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+              end
+
+              return vim.fn.getcwd() .. "/jest.config.ts"
+            end,
             env = { CI = true },
             cwd = function(path)
               return vim.fn.getcwd()
