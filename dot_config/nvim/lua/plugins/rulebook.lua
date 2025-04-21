5ca466d3b73da3655e43ef20c455e89e7b5b599b
Nuno Rodrigues <Nuno.Rodrigues@ctw.bmwgroup.com>
2025-04-21 11:49:24 +0100

    Add .config/nvim/lua/plugins/rulebook.lua

---
+++ dot_config/nvim/lua/plugins/rulebook.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "chrisgrieser/nvim-rulebook",
+    keys = {
+      {
+        "<leader>ri",
+        function()
+          require("rulebook").ignoreRule()
+        end,
+        desc = "Ignore rule (Rulebook)",
+      },
+      {
+        "<leader>rl",
+        function()
+          require("rulebook").lookupRule()
+        end,
+        desc = "Lookup rule (Rulebook)",
+      },
+      -- rule for yank diagnostic code
+      {
+        "<leader>ry",
+        function()
+          require("rulebook").yankDiagnosticCode()
+        end,
+        desc = "Yank diagnostic code (Rulebook)",
+      },
+      {
+        "<leader>rf",
+        function()
+          require("rulebook").suppressFormatter()
+        end,
+        desc = "Suppress formatter (Rulebook)",
+        mode = { "n", "x" },
+      },
+    },
+  },
+}
